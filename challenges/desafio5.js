a5374f9a3385b114c4fa3106d062dbc2da3326d3
Rafael Romano <rafa-kun@hotmail.com>
2021-11-23 14:41:01 -0300

    :white_check_mark: Adicionando requisitos 4,5,6
    Pequenos commits e vamos la

---
+++ challenges/desafio5.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  { nome: { $nin: ["McChicken"] } },
+  {
+    $addToSet: {
+      ingredientes: "ketchup",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, _id: 0, ingredientes: 1 },
+);
