ff0a26ee2e62b367f0fdc0059e49433b55afc4b2
Rafael Romano <rafa-kun@hotmail.com>
2021-11-23 19:15:54 -0300

    :white_check_mark: adicionando requisitos 10 a 22
    BOMBAAAAAA

---
+++ challenges/desafio12.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      valoresNutricionais: {
+        $each: [],
+        $sort: { percentual: -1 },
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, _id: 0, valoresNutricionais: 1 },
+);
