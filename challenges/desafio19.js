914fdc1aa707907524dff6dd91938d1c4eb27c68
Rafael Romano <rafa-kun@hotmail.com>
2021-11-23 21:29:20 -0300

    :ambulance: Testando ver se a correção funcionou
    vqv

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,11 +1,11 @@
-db.produtos.updateMany(
-  {},
-  { $rename: {
-    descricao: "descricaoSite",
-  } },
-);
+// db.produtos.updateMany(
+//   {},
+//   { $rename: {
+//     descricao: "descricaoSite",
+//   } },
+// );
 
-db.produtos.find(
-  {},
-  { nome: 1, descricaoSite: 1, _id: 0, descricao: 1 },
-);
+// db.produtos.find(
+//   {},
+//   { nome: 1, descricaoSite: 1, _id: 0, descricao: 1 },
+// );
